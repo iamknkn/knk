44e767cb1cc363ba063e2bfce9531e4c2c4cf65b
iamknkn <iamknaveenkumar@gmail.com>
2020-01-15 19:19:32 +0530

    first cpp file

---
+++ knk.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+/*	pair<int,char> pair1;
+	pair1.first = 100;
+	pair1.second = 'q';
+cout<<pair1.first<<" "<<pair1.second<<endl;*/
+
+	vector <int> g1;
+	g1.push_back(10);
+	for (int x:g1)
+	cout<<x;
+}
